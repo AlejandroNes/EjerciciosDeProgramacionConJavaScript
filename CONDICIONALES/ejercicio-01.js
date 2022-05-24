1656df13265d6863f5c14fe42a21b20382d14ba8
AlejandroNes <alejandro315595@mail.com>
2022-05-23 23:42:03 -0300

    Ejercicios de Programación con JS

---
+++ CONDICIONALES/ejercicio-01.js
@@ -0,0 +1,9 @@
+//Pedir un número e indicar si es  un número positivo o negativo
+
+let numero = Number(prompt("Ingrese un número"));
+
+if( numero > 0 ){
+    console.log(`El número ${numero} es positivo`)
+}else{
+    console.log(`El número ${numero} es negativo`)
+}
